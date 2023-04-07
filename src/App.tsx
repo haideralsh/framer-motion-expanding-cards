65a8161b304497a408e8a36ed49c3879c86c914a
Haider Alshamma <haidera@nulogy.com>
2023-04-07 15:49:43 -0400

    Use tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,95 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 import { HTMLMotionProps, motion } from "framer-motion";
 
 function App() {
-  const [openIndex, setOpenIndex] = useState(1)
+  const [openIndex, setOpenIndex] = useState(1);
 
   return (
-    <div>
-      <h1>Arcade Lobby</h1>
+    <div className="p-8 grid auto-rows-min w-full gap-5">
+      <h1 className="text-white text-6xl p-8">Arcade Lobby</h1>
 
-      <div className="App">
-        <Card
-          title="Mystic Realms"
-          subtitle=" A fantasy adventure game where players explore a mystical world full of magic and danger."
-          onClick={() => { setOpenIndex(1) }}
-          open={openIndex === 1} />
+      <Card
+        title="Mystic Realms"
+        color="bg-[#25d0ab]"
+        subtitle=" A fantasy adventure game where players explore a mystical world full of magic and danger"
+        onClick={() => {
+          setOpenIndex(1);
+        }}
+        open={openIndex === 1}
+      />
 
-        <Card
-          title="Galactic Conquest"
-          subtitle="A sci-fi strategy game where players command their own interstellar empire."
-          onClick={() => { setOpenIndex(2) }}
-          open={openIndex === 2} />
+      <Card
+        title="Galactic Conquest"
+        subtitle="A sci-fi strategy game where players command their own interstellar empire"
+        color="bg-[#ff7054]"
+        onClick={() => {
+          setOpenIndex(2);
+        }}
+        open={openIndex === 2}
+      />
 
-        <Card
-          title="Dungeon Delve"
-          subtitle="A dungeon-crawling RPG where players create their own adventurer and explore dangerous underground labyrinths."
-          onClick={() => { setOpenIndex(3) }}
-          open={openIndex === 3} />
-      </div>
+      <Card
+        title="Dungeon Delve"
+        color="bg-[#52a9ff]"
+        subtitle="A dungeon-crawling RPG where players create their own adventurer and explore dangerous underground labyrinths"
+        onClick={() => {
+          setOpenIndex(3);
+        }}
+        open={openIndex === 3}
+      />
     </div>
-  )
+  );
 }
 
-function Card({ open, title, subtitle, ...rest }: { open: boolean; title: string; subtitle: string } & HTMLMotionProps<"div">) {
-  return <motion.div
-    transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-    layout
-    className={open ? "card open-card" : "card closed-card"}
-    {...rest}>
+function Card({
+  open,
+  title,
+  subtitle,
+  color,
+  ...rest
+}: {
+  open: boolean,
+  title: string,
+  subtitle: string,
+  color: string
+} & HTMLMotionProps<"div">) {
+  let cardClasses = `flex flex-col content-start items-start cursor-pointer overflow-hidden p-4 rounded-xl`
 
-    <motion.h2
+  return (
+    <motion.div
       transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-      layout>{title}
-    </motion.h2>
-    <motion.p
-      transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-      layout>{subtitle}
-    </motion.p>
+      layout
+      className={open ? cardClasses + ` p-8 rounded-2xl ${color}` : cardClasses + ` mx-4 bg-zinc-900 text-zinc-500`}
+      {...rest}
+    >
+      <motion.h2
+        className={open ? "text-3xl mb-1" : "text-2xl"}
+        transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+        layout
+      >
+        {title}
+      </motion.h2>
+      <motion.p
+        className={open ? "font-sans opacity-60" : "font-sans"}
+        transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+        layout
+      >
+        {subtitle}
+      </motion.p>
 
-    {open && (
-      <motion.button
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ type: "ease", ease: "easeInOut", duration: 0.3 }}
-        layout>Join Game
-      </motion.button>
-    )}
-  </motion.div>
+      {open && (
+        <motion.button
+          className="mt-8 ml-auto text-white bg-black rounded-full text-2xl px-12 py-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ type: "ease", ease: "easeInOut", duration: 0.3 }}
+          layout
+        >
+          Join Game
+        </motion.button>
+      )}
+    </motion.div>
+  );
 }
 
-export default App
+export default App;
